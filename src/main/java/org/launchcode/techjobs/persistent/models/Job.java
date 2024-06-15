e5d6f969566a990ba4f2024c951358229a7b29a9
Aswini Raghul <aswiniraghul@gmail.com>
2024-06-15 14:55:47 -0500

    Updated GA#4

--- src/main/java/org/launchcode/techjobs/persistent/models/Job.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -9,8 +9,10 @@ import java.util.List;
 public class Job extends AbstractEntity {
 
     @ManyToOne
+    @JoinColumn(name = "employer_id")
     private Employer employer;
     @ManyToMany
+    @JoinColumn(name = "skills")
     private List<Skill> skills;
 
 
